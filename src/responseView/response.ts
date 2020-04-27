aa517a278996a0bd731a69764811f0139fe0945c
parthpatne <parthpatne@hotmail.com>
2020-04-27 16:48:28 +0530

    Response View

--- src/responseView/response.ts
+++ src/responseView/response.ts
@@ -1,21 +1,118 @@
-// import * as _ from 'lodash';
-// const _ = require("lodash")
 
-//   function component() {
-//     const element = document.createElement('div');
+import * as ActionSDK from 'actionSDK2';
+ActionSDK.APIs.actionViewDidLoad(true /*success*/);
 
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// Fetching HTML Elements in Variables by ID.
+var root = document.getElementById("root");
+let row = {};
+let actionInstance:  ActionSDK.ActionInstance = null;
 
-//     return element;
-//   }
+initialize();
 
-//   document.body.appendChild(component());
+function createBody(){
+    var title = document.createElement('h3');
+    title.innerHTML = actionInstance.title;
+    root.appendChild(title);
+    createQuestionView();
 
-// import '../creationView.html';
-// import '../updateView.html';
+    var submit = document.createElement("BUTTON");   // Create a <button> element
+    submit.innerHTML = "Submit";
+    submit.style.float = "right";
+    submit.addEventListener("click", function () {
 
-function response(name:String){
-	return name;
+     submitForm();
+    });
+
+    root.appendChild(submit);
+}
+function submitForm() {
+  
+  //parth check how to get row and id 
+  let actionInstanceRow: ActionSDK.ActionInstanceRow = {
+    id: "",
+    isUpdate: false,
+    row: row
+};
+
+
+ActionSDK.Utils.announceText(ActionSDK.Localizer.getString("SubmittingResponse"));
+        ActionSDK.ActionUtils.prepareActionInstanceRow(actionInstanceRow);
+        ActionSDK.APIs.createOrUpdateActionInstanceRows(actionInstance.id, [actionInstanceRow])
+            .then((success: boolean) => {
+                    ActionSDK.Utils.announceText(ActionSDK.Localizer.getString("Submitted"));
+                    ActionSDK.APIs.dismissScreen();
+                
+            }).catch(error => {
+                console.log("submit failed: "+ error.toString());
+            });
 }
 
-console.log(response("Test Html")) ;
+
+function createQuestionView(){
+
+  var count = 1;
+  actionInstance.columns.forEach((column: ActionSDK.ActionInstanceColumn) => {
+    
+          var qDiv = document.createElement("div");
+
+          var linebreak = document.createElement('br');
+          qDiv.appendChild(linebreak);  
+
+          var questionHeading = document.createElement('h4'); // Heading of For
+          questionHeading.innerHTML = count + "."+ column.title;
+          qDiv.appendChild(questionHeading);      
+
+          //add radio button
+          column.options.forEach((option:ActionSDK.ActionInstanceColumnOption) => {
+           var radioOption = getRadioButton(option.title,column.id,option.id);
+           qDiv.appendChild(radioOption);
+           
+          });
+          root.appendChild(qDiv);
+          count++;
+  });
+
+}
+
+function getRadioButton( text,name,id) {
+
+    var oDiv = document.createElement("div");
+    oDiv.id = id;
+	oDiv.setAttribute("columnId", name);
+	
+		oDiv.addEventListener("click", function () {
+		radiobuttonclick(this.id,this.getAttribute("columnId"));
+		});
+    var radiobox = document.createElement('input');
+    radiobox.type = 'radio';
+    radiobox.name = name;
+    radiobox.id = id;
+    radiobox.attributes
+  
+    oDiv.appendChild(radiobox);
+    oDiv.appendChild(document.createTextNode(text));    
+  
+    var newline = document.createElement('br');
+  
+
+    oDiv.appendChild(newline);
+
+    return oDiv;  
+} 
+
+function radiobuttonclick(optionId,colomnId){
+  console.log("parth radio button clicked: "+optionId + colomnId);
+  row[colomnId]=optionId;
+}
+function initialize(){
+
+    ActionSDK.APIs.getCurrentContext()
+    .then((context: ActionSDK.ActionContext) => {   
+      ActionSDK.APIs.getActionInstance(context.actionInstanceId)
+      .then((ai: ActionSDK.ActionInstance) => {
+      actionInstance = ai;
+      createBody();
+      })      
+    });
+
+}
